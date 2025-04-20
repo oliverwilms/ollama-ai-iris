00674aa2c026918598de7dd66dee842ccdd8885c
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2025-04-20 18:55:19 -0500

    url = f"iris://_SYSTEM:SYS@localhost:53795/IRISAPP"

--- test_conn.py
+++ test_conn.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-url = f"iris://teste:teste@localhost:51774/TESTE"
+url = f"iris://_SYSTEM:SYS@localhost:53795/IRISAPP"
 engine = create_engine(url)
 with engine.connect() as conn:
     print(conn.execute(text("SELECT 'hello world!'")).first()[0])
